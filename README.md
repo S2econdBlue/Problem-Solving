2e37a2c31e860507b891d87b458501d8ce9df0de
이청 <s2econd.blue@gmail.com>
2022-11-23 15:19:31 +0900

    README 수정 및 파일 정리

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 해결한 알고리즘을 기록합니다.
 
 1. 22/03/20
-[BOJ_3649_G4_로봇프로젝트](./BOJ/BOJ_3649_G4_%EB%A1%9C%EB%B4%87%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8.java) <https://www.acmicpc.net/problem/3649>
+[BOJ_3649_G4_로봇프로젝트](./BOJ/BOJ_3649_G4_%EB%A1%9C%EB%B4%87%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8.java) 
+<https://www.acmicpc.net/problem/3649>
 <br/>
 2. 22/03/27
-[BOJ_G4_17404_RGB거리2](./BOJ/BOJ_17404_G4_RGB%EA%B1%B0%EB%A6%AC2.java) <https://www.acmicpc.net/problem/17404>
+[BOJ_G4_17404_RGB거리2](./BOJ/BOJ_17404_G4_RGB%EA%B1%B0%EB%A6%AC2.java)
+<https://www.acmicpc.net/problem/17404>
 <br/>
 3. 22/04/04
 [BOJ_11404_G4_플로이드](./BOJ/BOJ_11404_G4_%ED%94%8C%EB%A1%9C%EC%9D%B4%EB%93%9C.java)
@@ -17,8 +19,12 @@
 <https://www.acmicpc.net/problem/11559>
 <br/>
 5. 22/04/07
-[BOJ_G2_청소년상어](./BOJ/BOJ_G2_%EC%B2%AD%EC%86%8C%EB%85%84%EC%83%81%EC%96%B4.java)<https://www.acmicpc.net/problem/19236>
+[BOJ_G2_청소년상어](./BOJ/BOJ_G2_%EC%B2%AD%EC%86%8C%EB%85%84%EC%83%81%EC%96%B4.java)
+<https://www.acmicpc.net/problem/19236>
 <br/>
 6. 22/11/22
 [BOJ_G2_피자판매](./BOJ/BOJ_G2_%ED%94%BC%EC%9E%90%ED%8C%90%EB%A7%A4.java)
 <https://www.acmicpc.net/problem/2632>
+7. 22/11/23
+[BOJ_G2_합이 0인 네 정수](./%EB%B0%B1%EC%A4%80/Gold/7453.%E2%80%85%ED%95%A9%EC%9D%B4%E2%80%850%EC%9D%B8%E2%80%85%EB%84%A4%E2%80%85%EC%A0%95%EC%88%98/%ED%95%A9%EC%9D%B4%E2%80%850%EC%9D%B8%E2%80%85%EB%84%A4%E2%80%85%EC%A0%95%EC%88%98.java)
+<https://www.acmicpc.net/problem/7453>
--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BOJ_G2_합이0인네정수 {
 	// 네 배열 중 하나씩 합쳤을 때 0이 나오는 쌍 개수
 	// 완전 탐색은 시간 초과 확실
-	// DP 방식으로 저장하는 방식이 필요할 것 같은데
+	// DP 방식으로 저장하는 방식이 필요할 것 같은데...
 
-	// 1차: 모든 조합을 확인 후 나오는 조합을 저장, 겹칠 수 있기 때문에 map에 저장
+	// 1차: 모든 조합을 확인 후 나오는 조합을 저장, 겹칠 수 있기 때문에 map에 저장 => 실패
+	// 2차: +,- 구분해서 2^28 내의 숫자를 배열에 저장하려 했으나 heap 터져서 실패
 
-	// 2차: RGB 거리같은건가?
+	// 해당 값 중 가장 작은 위치를 반환
 	static int getLowerPos(long[] arr, long V) {
 		int left = 0, right = arr.length;
 		while (left < right) {
@@ -23,6 +24,7 @@ public class Main {
 		return left;
 	}
 
+	// 해당 값 중 가장 큰 위치 반환
 	static int getUpperPos(long[] arr, long V) {
 		int left = 0, right = arr.length;
 		while (left < right) {
@@ -53,7 +55,7 @@ public class Main {
 		long[] arr1 = new long[n * n];
 		long[] arr2 = new long[n * n];
 
-		// 4000 * 4000
+		// 최대 4000 * 4000
 		int cnt = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
@@ -61,15 +63,17 @@ public class Main {
 				cnt++;
 			}
 		}
+		
+		// 최대 최대 4000 * 4000
 		cnt = 0;
-		// 4000 * 4000
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				arr2[cnt] = bd[i][2] + bd[j][3];
 				cnt++;
 			}
 		}
-
+		
+		// 이분탐색 위해 정렬
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
 
@@ -78,13 +82,17 @@ public class Main {
 		int arr1Pos = 0, arr2Pos = 0;
 		long answer = 0;
 		int arr1Right = 0;
+		
+		// arr1의 모든 값을 확인해야 함
 		while (arr1Right < n * n) {
 			// arr1의 lower, upper 구하기
+			// 
 			long arr1Left = getLowerPos(arr1, arr1[arr1Pos]);
 			arr1Right = getUpperPos(arr1, arr1[arr1Pos]);
 			long arr1Size = arr1Right - arr1Left;
 			long arr2Right = getUpperPos(arr2, -arr1[arr1Pos]);
 			long arr2Left = getLowerPos(arr2, -arr1[arr1Pos]);
+			// answer이 int형으로 담지 못할 수 있으므로 long형
 			answer += arr1Size * (arr2Right - arr2Left);
 			arr1Pos = arr1Right;
 		}
