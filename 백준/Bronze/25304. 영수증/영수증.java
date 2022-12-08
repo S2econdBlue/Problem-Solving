0ec4be3f96c989f24dfe34845d5bbb8d7be829d4
이청 (Chung Lee) <s2econd.blue@gmail.com>
2022-12-09 02:00:21 +0900

    [Bronze V] Title: 영수증, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			sum += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+		}
+		if (sum == cnt)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
