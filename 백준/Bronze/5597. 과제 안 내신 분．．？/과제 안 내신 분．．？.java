8164713d560e9aa3de9f7e73fb2d5bfb1bc1e841
이청 (Chung Lee) <s2econd.blue@gmail.com>
2022-12-15 02:53:57 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean[] arr = new boolean[31];
+		for (int i = 0; i < 28; i++) {
+			arr[Integer.parseInt(br.readLine())] = true;
+		}
+
+		for (int i = 1; i < 31; i++) {
+			if (!arr[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+}
